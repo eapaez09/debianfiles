6aa8d82750ddbbc890bc516a44b68cff93f2c479
pzeadrian <pzeadrian@duck.com>
2022-12-14 16:00:20 -0500

    update debian files

---
+++ 2_GUI/general/xorg-xinit/README.md
@@ -0,0 +1,4 @@
+You should run some commands for leave this xinitrc as the default.
+```
+sudo ln -s $HOME/.config/xorg-xinit/xinitrc ~/.xinitrc
+```
---
+++ 2_GUI/i3_sync.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/sh
+
+cd $HOME/Documents/debian/
+git pull
+
+cd
+
+if [[ $(hostnamectl hostname) == "hp" ]]; then
+    cp $HOME/.xprofile $HOME/Documents/debian/2_GUI/1920x1080/xprofile
+
+elif [[ $(hostnamectl hostname) == "asus" ]]; then
+    cp $HOME/.xprofile $HOME/Documents/debian/2_GUI/1366x768/xprofile
+else
+    echo "Skipping sync, unknown hostname"
+fi
+
+cp -R $HOME/.config/fish/ $HOME/Documents/debian/2_GUI/general/
+cp -R $HOME/.config/picom/ $HOME/Documents/debian/2_GUI/general/
+cp -R $HOME/.config/xorg-xinit/ $HOME/Documents/debian/2_GUI/general/
+cp -R $HOME/.config/rofi/ $HOME/Documents/debian/2_GUI/general/
+cp $HOME/i3_sync.sh $HOME/Documents/debian/2_GUI/
+
+
+cd $HOME/Documents/debian/
+git add --all
+git commit -m "update debian files"
+git push
+
+cd
