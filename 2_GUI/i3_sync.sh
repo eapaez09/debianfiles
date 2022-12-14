038f649e1d1300a09c4f0cb6dec9c7eee87d34ca
pzeadrian <pzeadrian@duck.com>
2022-12-14 16:16:04 -0500

    update debian files

--- 2_GUI/i3_sync.sh
+++ 2_GUI/i3_sync.sh
@@ -8,12 +8,12 @@ cd
 if [ "$hostname" = "hp" ] 
 then
     cp $HOME/.xprofile $HOME/Documents/debian/2_GUI/1920x1080/
-    mv $HOME/Documents/debian/2_GUI/1920x1080/.xprofile $HOME/Documents/debian/2_GUI/1920x1080/xprofile
+    # mv $HOME/Documents/debian/2_GUI/1920x1080/.xprofile $HOME/Documents/debian/2_GUI/1920x1080/xprofile
 
 elif [ "$hostname" = "asus" ]
 then
     cp $HOME/.xprofile $HOME/Documents/debian/2_GUI/1366x768/
-    mv $HOME/Documents/debian/2_GUI/1366x768/.xprofile $HOME/Documents/debian/2_GUI/1366x768/xprofile
+    # mv $HOME/Documents/debian/2_GUI/1366x768/.xprofile $HOME/Documents/debian/2_GUI/1366x768/xprofile
 else
     echo "Skipping sync, unknown hostname"
 fi
